1fa6bc5cbb14a4a168688c2a72070a831b850c91
sambantham <76392855+sambantham-p@users.noreply.github.com>
2022-05-21 16:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-
 const API_KEY = 'api_key=42f17cae410e872666dad6a2825dcc72';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL +'/discover/movie?sort_by=popularity.desc&'+API_KEY;
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const IMG_URL = 'https://image.tmdb.org/t/p/original';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
-const gener ='https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US';
+const gener ='https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US';  
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const series='https://api.themoviedb.org/3/tv/{tv_id}/season/{season_number}?api_key=<<api_key>>&language=en-US'
+
+const requests = {
+    fetchNetflixOrignals: `${BASE_URL}/discover/tv?${API_KEY}&with_networks=213`,
+    fetchPopular: `${BASE_URL}/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&${API_KEY}`,
+    fetchTrending: `${BASE_URL}/trending/all/week${API_KEY}&language=en=US`,
+    fetchActionMovies: `${BASE_URL}/discover/movie?=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `${BASE_URL}/discover/movie?=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `${BASE_URL}/discover/movie?=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `${BASE_URL}/discover/movie?=${API_KEY}&with_genres=35`,
+};
+
 getMovie(API_URL);
 
 
@@ -69,3 +80,23 @@ form.addEventListener('submit',(e)=>{
     }
 
 })
+// used to truncate the string 
+function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+  // banner
+  fetch(requests.fetchNetflixOrignals)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.results);
+    // every refresh the movie will be change
+    const setMovie = data.results[Math.floor(Math.random() * data.results.length - 1)];
+    console.log(setMovie);
+    var banner = document.getElementById("banner");
+    var banner_title = document.getElementById("banner__title");
+    var banner__desc = document.getElementById("banner__description");
+    banner.style.backgroundImage = "url(" + IMG_URL + setMovie.backdrop_path + ")";
+    banner__desc.innerText = truncate(setMovie.overview, 150);
+    banner_title.innerText = setMovie.name;
+  })
+  
